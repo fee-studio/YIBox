ba7307604e320507ddd69c04d6c480b45c0e07bb
iosfeng <iosfeng@163.com>
2016-01-30 22:07:22 +0800

    init project code

---
+++ YIBox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YIBox
+//
+//  Created by efeng on 16/1/30.
+//  Copyright © 2016年 buerguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YIBox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YIBox
+//
+//  Created by efeng on 16/1/30.
+//  Copyright © 2016年 buerguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
